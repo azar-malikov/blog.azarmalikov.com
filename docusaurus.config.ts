79ea8b0b251fb6120ce72bd97bed91564cc5f66e
Azar Malikov <melikovazer64@gmail.com>
2025-11-12 04:04:16 +0400

    Config: Updated GitHub deployment settings

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -18,12 +18,14 @@ const config: Config = {
   url: 'https://your-docusaurus-site.example.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/blog.azarmalikov.com/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'azarmalikov', // Usually your GitHub org/user name.
+  projectName: 'blog.azarmalikov.com', // Usually your repo name.
+
+  deploymentBranch: 'gh-pages',\n  trailingSlash: false,
 
   onBrokenLinks: 'throw',
 
